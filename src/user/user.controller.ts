e8a9c494b4265eb7fa325e3a34047918102c941d
Rus0d <rusya.liamg@gmail.com>
2021-12-21 17:22:45 +0200

    added login method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import {Body, Controller, Post, UsePipes, ValidationPipe} from "@nestjs/common";
 
 import {UserService} from "@app/user/user.service";
 import {CreateUserDto} from "@app/user/dto/createUser.dto";
+import {LoginUserDto} from "@app/user/dto/loginUser.dto";
 import {UserResponseInterface} from "@app/user/types/userResponse.interface";
 
 @Controller()
@@ -12,6 +13,13 @@ export class UserController {
     @UsePipes(new ValidationPipe())
     async createUser(@Body("user") crateUserDto: CreateUserDto): Promise<UserResponseInterface> {
         const user = await this.userService.createUser(crateUserDto);
+        return this.userService.buildUserResponse(user);
+    }
+
+    @Post("/user/login")
+    @UsePipes(new ValidationPipe())
+    async login(@Body("user") loginUserDto: LoginUserDto): Promise<UserResponseInterface> {
+        const user = await this.userService.login(loginUserDto);
         return this.userService.buildUserResponse(user);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,12 @@ import {Repository} from "typeorm";
 import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {sign} from "jsonwebtoken";
+import {compare} from "bcrypt";
+
 
 import {JWT_SECRET} from "@app/config";
 import {CreateUserDto} from "@app/user/dto/createUser.dto";
+import {LoginUserDto} from "@app/user/dto/loginUser.dto";
 import {UserEntity} from "@app/user/user.entity";
 import {UserResponseInterface} from "@app/user/types/userResponse.interface";
 
@@ -29,6 +32,21 @@ export class UserService {
         const newUser = new UserEntity();
         Object.assign(newUser, crateUserDto);
         return await this.userRepository.save(newUser);
+    }
+
+    async login(getUserDto: LoginUserDto): Promise<UserEntity> {
+        const userByEmail = await this.userRepository.findOne({
+            email: getUserDto.email
+        }, {select: ["id", "username", "email", "bio", "image", "password"]});
+        let match = false;
+        if (userByEmail) {
+            match = await compare(getUserDto.password, userByEmail.password);
+        }
+        if (match) {
+            delete userByEmail.password
+            return userByEmail;
+        }
+        throw new HttpException("Wrong password or email", HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     generateJwt(user: UserEntity): string {
